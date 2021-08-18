06db76bc326d5389a22e09fe12d18188c3b7ef2a
Saoussen Slii <38138018+sawsanCS@users.noreply.github.com>
2021-08-18 15:16:21 -0400

    adding dispatch to buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useSelector } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 function App() {
   const counter = useSelector((state) => state.counter);
   const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <div>Counter : {counter}</div>
-      <button>+</button>
-      <button>-</button>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
       <div>isLogged : {isLogged.toString()}</div>
     </div>
   );
